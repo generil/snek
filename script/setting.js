7e098d55f9114a77b369761d7c2b53c5a61551e4
Generil de los Santos <generild@gmail.com>
2017-04-21 14:41:13 +0800

    added the details

---
+++ script/setting.js
@@ -0,0 +1,10 @@
+var canvas = document.getElementById('snek');
+var context = canvas.getContext('2d');
+
+var snakeSize = 10;
+// var w = 350;
+// var h = 350;
+var score = 0;
+var snake;
+var snakeSize = 10;
+var food;
