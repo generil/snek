4eb0894bc8f5830a84db58ee7f19dc7d4846fcf4
Generil de los Santos <generild@gmail.com>
2017-04-21 14:49:51 +0800

    some changes

--- script/setting.js
+++ script/setting.js
@@ -2,7 +2,7 @@ var canvas = document.getElementById('snek');
 var context = canvas.getContext('2d');
 
 var snakeSize = 10;
-// var w = 350;
+var w = 350;
 // var h = 350;
 var score = 0;
 var snake;
