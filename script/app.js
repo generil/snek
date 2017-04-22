c07d303058ed4847e6f8242e84f37e0f70a78014
Generil de los Santos <generild@gmail.com>
2017-04-22 15:33:29 +0800

    beautified code

--- script/app.js
+++ script/app.js
@@ -1,44 +1,44 @@
-(function (window, document, drawModule, undefined) {
-	var btn = document.getElementById('btn');
+(function(window, document, drawModule, undefined) {
+    var btn = document.getElementById('btn');
 
-	btn.addEventListener("click", function(){
-		drawModule.init();
-	});
+    btn.addEventListener("click", function() {
+        drawModule.init();
+    });
 
-	document.onkeydown = function(event) {
-		keyCode = window.event.keyCode;
-		keyCode = event.keyCode;
+    document.onkeydown = function(event) {
+        keyCode = window.event.keyCode;
+        keyCode = event.keyCode;
 
-		switch(keyCode) {
+        switch (keyCode) {
 
-			case 37:
-			if (direction != 'right') {
-				direction = 'left';
-			}
-			console.log('left');
-			break;
+            case 37:
+                if (direction != 'right') {
+                    direction = 'left';
+                }
+                console.log('left');
+                break;
 
-			case 39:
-			if (direction != 'left') {
-				direction = 'right';
-				console.log('right');
-			}
-			break;
+            case 39:
+                if (direction != 'left') {
+                    direction = 'right';
+                    console.log('right');
+                }
+                break;
 
-			case 38:
-			if (direction != 'down') {
-				direction = 'up';
-				console.log('up');
-			}
-			break;
+            case 38:
+                if (direction != 'down') {
+                    direction = 'up';
+                    console.log('up');
+                }
+                break;
 
-			case 40:
-			if (direction != 'up') {
-				direction = 'down';
-				console.log('down');
-			}
-			break;
-		}
-	}
+            case 40:
+                if (direction != 'up') {
+                    direction = 'down';
+                    console.log('down');
+                }
+                break;
+        }
+    }
 
 })(window, document, drawModule);
--- script/draw.js
+++ script/draw.js
@@ -1,34 +1,37 @@
-var drawModule = (function () {
+var drawModule = (function() {
 
     var bodysnek = function(x, y) {
         context.fillStyle = 'green';
-        context.fillRect(x*snekSize, y*snekSize, snekSize, snekSize);
+        context.fillRect(x * snekSize, y * snekSize, snekSize, snekSize);
         context.strokeStyle = 'darkgreen';
-        context.strokeRect(x*snekSize, y*snekSize, snekSize, snekSize);
+        context.strokeRect(x * snekSize, y * snekSize, snekSize, snekSize);
     }
 
     var pizza = function(x, y) {
         context.fillStyle = 'yellow';
-        context.fillRect(x*snekSize, y*snekSize, snekSize, snekSize);
+        context.fillRect(x * snekSize, y * snekSize, snekSize, snekSize);
         context.fillStyle = 'red';
-        context.fillRect(x*snekSize+1, y*snekSize+1, snekSize-2, snekSize-2);
+        context.fillRect(x * snekSize + 1, y * snekSize + 1, snekSize - 2, snekSize - 2);
     }
 
     var scoreText = function() {
         var score_text = "Score: " + score;
         context.fillStyle = 'blue';
-        context.fillText(score_text, 145, h-5);
+        context.fillText(score_text, 145, h - 5);
     }
 
     var drawsnek = function() {
         var length = 4;
         snek = [];
-        for (var i = length-1; i>=0; i--) {
-            snek.push({x:i, y:0});
+        for (var i = length - 1; i >= 0; i--) {
+            snek.push({
+                x: i,
+                y: 0
+            });
         }
     }
 
-    var paint = function(){
+    var paint = function() {
         context.fillStyle = 'lightgrey';
         context.fillRect(0, 0, w, h);
         context.strokeStyle = 'black';
@@ -45,22 +48,25 @@ var drawModule = (function () {
             snekX--;
         } else if (direction == 'up') {
             snekY--;
-        } else if(direction == 'down') {
+        } else if (direction == 'down') {
             snekY++;
         }
 
-        if (snekX == -1 || snekX == w/snekSize || snekY == -1 || snekY == h/snekSize || checkCollision(snekX, snekY, snek)) {
+        if (snekX == -1 || snekX == w / snekSize || snekY == -1 || snekY == h / snekSize || checkCollision(snekX, snekY, snek)) {
             //restart game
             btn.removeAttribute('disabled', true);
 
-            context.clearRect(0,0,w,h);
+            context.clearRect(0, 0, w, h);
             gameloop = clearInterval(gameloop);
             return;
         }
 
-        if(snekX == food.x && snekY == food.y) {
-            var tail = {x: snekX, y: snekY}; //Create a new head instead of moving the tail
-            score ++;
+        if (snekX == food.x && snekY == food.y) {
+            var tail = {
+                x: snekX,
+                y: snekY
+            }; //Create a new head instead of moving the tail
+            score++;
 
             createFood(); //Create new food
         } else {
@@ -71,7 +77,7 @@ var drawModule = (function () {
         //The snek can now eat the food.
         snek.unshift(tail); //puts back the tail as the first cell
 
-        for(var i = 0; i < snek.length; i++) {
+        for (var i = 0; i < snek.length; i++) {
             bodysnek(snek[i].x, snek[i].y);
         }
 
@@ -85,11 +91,11 @@ var drawModule = (function () {
             y: Math.floor((Math.random() * 30) + 1)
         }
 
-        for (var i=0; i>snek.length; i++) {
+        for (var i = 0; i > snek.length; i++) {
             var snekX = snek[i].x;
             var snekY = snek[i].y;
 
-            if (food.x===snekX && food.y === snekY || food.y === snekY && food.x===snekX) {
+            if (food.x === snekX && food.y === snekY || food.y === snekY && food.x === snekX) {
                 food.x = Math.floor((Math.random() * 30) + 1);
                 food.y = Math.floor((Math.random() * 30) + 1);
             }
@@ -97,14 +103,14 @@ var drawModule = (function () {
     }
 
     var checkCollision = function(x, y, array) {
-        for(var i = 0; i < array.length; i++) {
-            if(array[i].x === x && array[i].y === y)
-            return true;
+        for (var i = 0; i < array.length; i++) {
+            if (array[i].x === x && array[i].y === y)
+                return true;
         }
         return false;
     }
 
-    var init = function(){
+    var init = function() {
         direction = 'down';
         drawsnek();
         createFood();
@@ -112,6 +118,6 @@ var drawModule = (function () {
     }
 
     return {
-        init : init
+        init: init
     };
 }());
