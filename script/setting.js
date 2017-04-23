3d61898defd05c8ae9c097d84f7173d4cf6a0028
Generil de los Santos <generild@gmail.com>
2017-04-23 13:46:46 +0800

    refinements

--- script/setting.js
+++ script/setting.js
@@ -1,9 +1,9 @@
-var canvas = document.getElementById('mycanvas');
+var canvas = document.getElementById('snek');
 var context = canvas.getContext('2d');
 
 var snekSize = 10;
-var w = 350;
-var h = 350;
+var w = 500;
+var h = 400;
 var score = 0;
 var snek;
 var snekSize = 10;
